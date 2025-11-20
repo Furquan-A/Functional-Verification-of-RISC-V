cd10cae146ab61a1abc3731eb22aa0ace6f57a17
Furquan-A <furquan.741@gmail.com>
2025-11-19 17:33:35 -0800

    working on the ral files reg blocks

--- tb/ral_agent/msrv32_reg_block.sv
+++ tb/ral_agent/msrv32_reg_block.sv
@@ -21,4 +21,23 @@ class msrv32_reg_block extends uvm_reg_block;
 endclass : msrv32_reg_block
 
 class msrv32_reg_block_for_pc extends uvm_reg_block;
+	`uvm_object_utils(msrv32_reg_block_for_pc)
+	
+	rand pc_reg pc_h;
+	
+	uvm_reg_map rv32_reg_map; // As uvm_reg_map is non virtual we will directly create object for the base class 
+	
+	function new (string name = " msrv32_reg_block_for_pc",'h0,4,UVM_LITTLE_ENDIAN,0);
+	
+	pc_h = pc_reg::type_id::create("pc_h");
+	pc_h.configure(this,null,"");
+	pc_h.build();
+	
+	addd_hdl_path("msrv32_tb_top.DUT","RTL");
+	pc_h.add_hdl_path_slice("DUT.REG1.pc_out",0,32);
+	rv32_reg_map.add_reg(pc_h,5'h0,"RW");
+	lock_model();
+	
+endfunction
+	
 endclass :msrv32_reg_block_for_pc
---
+++ test/msrv32_test_pkg.sv
@@ -0,0 +1,142 @@
+class msrv32_test_base extends uvm_test;
+
+	`uvm_component_utils("msrv32_test_base")
+
+	msrv32_env env;
+	msrv32_env_config m_cfg;
+	uvm_status_e status; // Returns status for the register operations
+
+	bit has_data_agent = 1;
+	bit has_instr_agent = 1;
+	bit has_irq_agent = 1;
+	bit has_rst_agent = 1;
+	bit has_instr_subscriber = 1;
+	
+	int no_of_data_agents = 2;
+	int no_of_instr_agents = 2;
+	int no_of_irq_agents = 2;
+	int no_of_rst_agents = 2;
+	int a = 23;
+	int b;
+	
+	
+	msrv32_data_agent_config d_cfg[];
+	msrv32_instr_agent_config in_cfg[];
+	msrv32_irq_agent_config i_cfg[];
+	msrv32_rst_agent_config r_cfg[];
+	
+	msrv32_reg_block rv32_reg_block_h;
+	msrv32_reg_block_for_pc rv32_reg_block_h_for_pc;
+	
+	string test_case_name;
+	
+	extern function new(string name = "msrv32_test_base", uvm_component parent);
+	extern function void build_phase(uvm_phase phase);
+	extern function void config_riscv();
+	extern function void end_of_elaboration_phase(uvm_phase phase);
+	extern function void report_phase (uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+	
+endclass 
+
+
+//====================================== new =============================
+
+function msrv32_test_base :: new(string name = "msrv32_test_base" uvm_component parent);
+super.new(name,parent);
+endfunction
+
+//====================================== end_of_elaboration_phase ===================
+
+function void msrv32_test_base :: end_of_elaboration_phase(uvm_phase phase);
+super.end_of_elaboration_phase(phase);
+uvm_top.print_topology();
+endfunction 
+
+// ==================================== run task ====================================
+
+task msrv32_test_base :: run_phase(uvm_phase phase)l
+	begin
+		this.rv32_reg_block_h.reg_file.write(status, 15, a, .path(UVM_BACKDOOR), .map(rv32_reg_block_h.rv32_reg_map));
+		this.rv32_reg_block_h.reg_file.read(status, 15, b,.path(UVM_BACKDOOR), .map(rv32_reg_block_h.rv32_reg_map));
+		$display("this value of b is %0d",b);
+		end
+endtask 
+
+// ===================================== config ==========================
+
+function void msrv32_test_base :: config_riscv();
+
+	if(has_data_agent)
+		begin 
+			d_cfg = new[no_of_data_agents];
+			foreach(d_cfg[i])
+				begin 
+					d_cfg[i]= msrv32_data_agent_config ::type_id :: create($sformatf("d_cfg[%0d]",i),this);
+					
+					if(!uvm_config_db#(virtual msrv32_ahb_data_if)::get(this,"",$sformatf("msrv32_ahb_data_if_%0d",i),d_cfg[i].vif))
+						`uvm_fatal("DATA_CONFIG.WRITE","cannot get() the interface vif from the uvm_config_db. have you set() it?")
+						
+					d_cfg[i].is_active = UVM_ACTIVE;
+					m_cfg.d_cfg[i]=d_cfg[i];
+				end 
+		end 
+
+	if(has_instr_agent)
+		begin 
+			in_cfg = new[no_of_instr_agents];
+			foreach(in_cfg[i])
+				begin 
+					in_cfg[i]= msrv32_instr_config ::type_id :: create($sformatf("in_cfg[%0d]",i),this);
+						
+					if(!uvm_config_db#(virtual msrv32_ahb_instr_if)::get(this,"",$sformatf("msrv32_ahb_instr_if_%0d",i),in_cfg[i].vif))
+						`uvm_fatal("INSTR_CONFIG.WRITE","cannot get() the interface vif from the uvm_config_db. have you set() it?")
+							
+					in_cfg[i].is_active = UVM_ACTIVE;
+					m_cfg.in_cfg[i]=in_cfg[i];
+					end 
+			end 
+			
+	if(has_irq_agent)
+		begin 
+			i_cfg = new[no_of_instr_agents];
+			foreach(i_cfg[i])
+				begin 
+					i_cfg[i]= msrv32_irq_config ::type_id :: create($sformatf("i_cfg[%0d]",i),this);
+						
+					if(!uvm_config_db#(virtual msrv32_ahb_irq_if)::get(this,"",$sformatf("msrv32_ahb_irq_if_%0d",i),i_cfg[i].vif))
+						`uvm_fatal("INSTR_CONFIG.WRITE","cannot get() the interface vif from the uvm_config_db. have you set() it?")
+							
+					i_cfg[i].is_active = UVM_ACTIVE;
+					m_cfg.i_cfg[i]=i_cfg[i];
+					end 
+			end
+					
+	if(has_rst_agent)
+		begin 
+			r_cfg = new[no_of_rst_agents];
+			foreach(r_cfg[i])
+				begin 
+					r_cfg[i]= msrv32_irq_config ::type_id :: create($sformatf("r_cfg[%0d]",i),this);
+						
+					if(!uvm_config_db#(virtual msrv32_ahb_rst_if)::get(this,"",$sformatf("msrv32_ahb_rst_if_%0d",i),r_cfg[i].vif))
+						`uvm_fatal("INSTR_CONFIG.WRITE","cannot get() the interface vif from the uvm_config_db. have you set() it?")
+							
+					r_cfg[i].is_active = UVM_ACTIVE;
+					m_cfg.r_cfg[i]=r_cfg[i];
+					end 
+			end
+			
+	m_cfg.has_data_agent = has_data_agent;
+	m_cfg.has_instr_agent = has_instr_agent;
+	m_cfg.has_irq_agent = has_irq_agent;
+	m_cfg.has_rst_agent = has_rst_agent;
+	m_cfg.has_instr_subscriber = has_instr_subscriber;
+	
+	m_cfg.no_of_data_agents = no_of_data_agents;
+	m_cfg.no_of_instr_agents = no_of_instr_agents;
+	m_cfg.no_of_irq_agents = no_of_irq_agents;
+	m_cfg.no_of_rst_agents = no_of_rst_agents;
+	
+endfunction
+
