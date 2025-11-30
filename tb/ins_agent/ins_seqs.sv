8caeb86f6d438cc9ae470049c362143e882c1f3a
Furquan-A <furquan.741@gmail.com>
2025-11-30 01:04:27 -0800

    *

--- tb/ins_agent/ins_seqs.sv
+++ tb/ins_agent/ins_seqs.sv
@@ -3,7 +3,7 @@ class msrv32_instr_seqs_base extends uvm_sequence #(msrv32_instr_trans);
 	`uvm_object_utils(msrv32_instr_seqs_base)
 	
 	// Properties
-	msrv32_env_config m_cfg;
+	msrv32_env_config env_cfg;
 	uvm_status_e status;
 	msrv32_reg_block rv32_reg_block_h;
 	msrv32_reg_block_for_pc rv32_reg_block_h_for_pc;
@@ -16,9 +16,19 @@ class msrv32_instr_seqs_base extends uvm_sequence #(msrv32_instr_trans);
 endfunction 
 
 // ============================== New ============================================================
+
 function msrv32_instr_seqs_base :: new (string name = "msrv32_instr_seqs_base);
 	super.new(name);
 endfunction 
 
+// ============================ body ============================================================
 
+task msrv32_instr_seqs_base :: body();
+	if(!uvm_config_db #(msrv32_env_config) :: get(null,get_full_name(),"msrv32_env_config",env_cfg))
+		`uvm_fatal(get_full_name(),"configuration in instr_sequence base is not available, did you set() it ?")
+	
+	this.rv32_reg_block_h = env_cfg.rv32_reg_block_h;
+	this.rv32_reg_block_h_for_pc = env_cfg.rv32_reg_block_h_for_pc;
+
+endtask 
 	
