fd3ae227bcd60ae0531d70db822addf48bde21cf
Furquan-A <furquan.741@gmail.com>
2025-12-07 18:59:13 -0800

    instruction monitor

--- tb/ins_agent/ins_driver.sv
+++ tb/ins_agent/ins_driver.sv
@@ -8,6 +8,10 @@ class msrv32_instr_driver extends uvm_driver #(msrv32_instr_trans);
 	msrv32_env_config env_cfg;
 	msrv32_instr_agent_config in_cfg;
 	
+	bit [7:0] mem [bit[31;0]];
+	
+	virtual msrv32_ahb_instr_if.DRV_MP vif;
+	
 	// methods 
 	extern function new (string name = "msrv32_instr_driver", uvm_component parent);
 	extern function void build_phase(uvm_phase phase);
@@ -32,10 +36,62 @@ function void msrv32_instr_driver :: build_phase(uvm_phase phase);
 	if(!uvm_config_db#(msrvv32_instr_agent_config) :: get(this, "","msrv32_instr_agent_config",in_cfg))
 		`uvm_fatal("INSTR_CONFIG","cannot get the in_cfg from db. did you set() it ?")
 	
+	if(!uvm_config_db#(msrv32_env_config) :: get(this, "","msrv32_env_config",env_cfg))
+		`uvm_fatal(get_full_name(),"is not set in the referance model. set properly")
 endfunction 
 
 // ======================= connect_phase ========================================================
 
 function void msrv32_instr_driver :: connect_phase(uvm_phase phase);
 	super.connect_phase(phase);
-	vif = 
+	vif = in_cfg.vif;
+endfunction 
+
+// ===================== run_phase ==============================================================
+
+task msrv32_instr_driver :: run_phase(uvm_phase phase);
+	forever 
+		begin 
+			seq_item_port.get_next_item(req);
+			storage(req);
+			send_to_dut();
+			seq_item_port.item_done(req);
+		end 
+endtask 
+
+// ===================== storage ================================================================
+
+task msrv32_instr_driver :: storage(msrv32_instr_trans req);
+	begin 
+		$display("addr in driver = %0d", req.instr_addr);
+		mem[req.instr_addr + 32'h0] = req.instruction[7:0];
+		mem[req.instr_addr + 32'h1] = req.instruction[15:8];
+		mem[req.instr_addr + 32'h2] = req.instruction[23:16];
+		mem[req.instr_addr + 32'h3] = req.instruction[31:24];
+		
+		req.instr_addr = req.instr_addr = 32'h4;
+	end 
+endtask 
+
+// =================== send_to_dut ==============================================================
+
+task msrv32_instr_driver :: send_to_dut ();
+	begin 
+		wait(~((vif.drv_cb.ms_riscv32_mp_imaddr_out == 32'hz)||(vif.drv_cb.ms_riscv32_mp_imaddr_out == 32'hx)));
+		`uvm_info(get_full_name().$sformat("pc value im_addr_out from design : %0d".vif.drv_cb.ms_riscv32_mp_imaddr_out). UVM_LOW)
+		
+		env_cfg.pc_reg = vif.drv_cb.ms_riscv32_mp_imaddr_out;
+		
+		vif.drv_cb.ms_riscv32_mp_instr_hready_in<= 1'b1;
+		vif.drv_cb.ms_riscv32_mp_instr_in[7:0]=mem[vif.drv_cb.ms_riscv32_mp_imaddr_out + 32'h00000000];
+		vif.drv_cb.ms_riscv32_mp_instr_in[15:8]=mem[vif.drv_cb.ms_riscv32_mp_imaddr_out + 32'h00000000];
+		vif.drv_cb.ms_riscv32_mp_instr_in[23:16]=mem[vif.drv_cb.ms_riscv32_mp_imaddr_out + 32'h00000000];
+		vif.drv_cb.ms_riscv32_mp_instr_in[31:24]=mem[vif.drv_cb.ms_riscv32_mp_imaddr_out + 32'h00000000];
+		
+		@(vif.drv_cb);
+		
+		vif.drv_cb.ms_riscv32_mp_instr_in<=1'b0;
+		`uvm_info(get_full_name(),$sformat("instruction driver : %b " vif.drv_cb.ms_riscv32_mp_instr_in),UVM_LOW)
+	end 
+endtask
+		
--- tb/ins_agent/ins_monitor.sv
+++ tb/ins_agent/ins_monitor.sv
@@ -8,6 +8,7 @@ class msrv32_instr_monitor extends uvm_monitor;
 	
 	msrv32_instr_trans instr_xtn;
 	msrv32_instr_agent_config in_cfg;
+	
 	bit [31:0] instruction;
 	
 	// Interface 
@@ -27,10 +28,14 @@ class msrv32_instr_monitor extends uvm_monitor;
 	
 endclass 
 
+// ============================ new ====================================================================
+
 function msrv32_instr_monitor :: new(string name = "msrv32_instr_monitor", uvm_component parent);
 super.new(name,parent);
 instr_monitor_port = new("instr_monitor_port", this);
 endfunction 
+
+// ========================= build_phase ==============================================================
 
 function msrv32_instr_monitor :: build_phase(uvm_phase phase);
 super.build_phase(phase);
@@ -42,15 +47,146 @@ if(!uvm_config_db#(msrv32_instr_agent_config)::get(this,"","msrv32_instr_agent_c
 	
 endfunction : build_phase
 
+// ============================= connect_phase =========================================================
+
 function msrv32_instr_monitor :: connect_phase(uvm_phase phase);
-
-// Functionality 
-
+	
+	super.connect_phase(uvm_phase phase );
+	vif = in_cfg.vif;
 endfunction : connect_phase
 
+// ========================== run_phase ================================================================
 
 task msrv32_instr_monitor :: run_phase(uvm_phase phase);
-
-// functionality 
-
+	forever
+		begin
+			instruction[31:0] = vif.mon_cb.ms_riscv32_mp_instr_in;
+			if((!instruction==32'h00000013)|| instruction == 32'h0)
+				instr_xtn.nop = 1'b1;
+			else 
+				begin 
+					decode_instruction();
+				end
+			instr_monitor_port.write(instr_xtn);
+			@(vif.mon_cb);
+		end 
 endtask : run_phase
+
+// ======================= decode_instruction =========================================================
+
+function void msrv32_instr_monitor :: decode_instruction();
+	
+	case(instruction [6:0] ) // opcode 
+	7'b0010011: begin 
+					case(instruction[14:12] // funct3
+					3'b000: begin 
+								instr_xtn.cmd = ADDI;
+							end 
+					3'b100: begin 
+								instr_xtn.cmd = XORI;
+							end 
+					3'b110: begin 
+								instr_xtn.cmd = ORI;
+							end 
+					3'b111: begin 
+								instr_xtn.cmd = ANDI;
+							end 
+					3'001: begin 
+								instr_xtn.cmd = SLLI;
+							end 
+					3'b101: begin 
+								case(instruction[31:25])
+									7'b00000000: instr_xtn.cmd = SRLI;
+									7'b01000000: instr_xtn.cmd = SRAI;
+								endcase 
+							end
+					3'010: begin 
+								instr_xtn.cmd = SLTI;
+							end 
+					3'011: begin 
+								instr_xtn.cmd = SLTUI;
+							end 
+					endcase 
+					
+					instr_xtn.rd = instruction[11:7];
+					instr_xtn.rs1 = instruction[19:15];
+					instr_xtn.imm[11:0] = instruction[31:20];
+					instr_xtn.funct3=instruction[14:12];
+					`uvm_info(get_full_name(),"I type instructions ", UVM_DEBUG)
+				end 
+					
+	7'b0110011: begin // r - type 
+					case(instruction[14:12]) // funct3
+					3'b000: begin 
+								case(instruction[31:25])
+									7'b00000000: instr_xtn.cmd = ADD;
+									7'b01000000: instr_xtn.cmd = SUB;
+								endcase 
+							end 
+					3'b100: begin 
+								instr_xtn.cmd = XOR;
+							end 
+					3'b110: begin 
+								instr_xtn.cmd = OR;
+							end
+					3'b111: begin 
+								instr_xtn.cmd = AND;
+							end
+					3'b001: begin 
+								instr_xtn.cmd = SLL;
+							end
+					3'b101: begin 
+								case(instruction[31:25])
+									instr_xtn.cmd = SRL;
+									instr_xtn.cmd = SRA;
+								endcase
+							end
+					3'b010: begin 
+								instr_xtn.cmd = SLT;
+							end
+					3'b011: begin 
+								instr_xtn.cmd = SLTU;
+							end
+					endcase 
+					
+					instr_xtn.rd=instruction[11:7];
+					instr_xtn.rs1=instruction[19:15];
+					instr_xtn_rs2=instruction[24:20];
+					instr_xtn.funct7=instruction[31:25];
+					instr_xtn.funct3=instruction[14:12];
+					`uvm_info(get_full_name(),"R type instructions ", UVM_DEBUG)
+				
+				end
+				
+	7'b00000011: begin // l_type
+					case (instruction[14:12]) // funct3 
+					3'b000: instr_xtn.cmd = LB;
+					3'b001: instr_xtn.cmd = LH;
+					3'b010: instr_xtn.cmd = LW;
+					3'b100: instr_xtn.cmd = LBU;
+					3'b101: instr_xtn.cmd = LHU;
+					endcase 
+					
+					instr_xtn.rd=instruction[11:7];
+					instr_xtn.rs1=instruction[19:15];
+					instr_xtn.imm[11:0]=instruction[31:20];
+					instr_xtn.funct3=instruction[14:12];
+					`uvm_info(get_full_name(),"LOAD type instructions ", UVM_DEBUG)
+					
+				end 
+				
+	7'b0100011: begin // s-type
+					case (instruction[14:0]) // funct3 
+					3'b000: instr_xtn.cmd=SB;
+					3'b001: instr_xtn.cmd=SH;
+					3'b010: instr_xtn.cmd=SW;
+					endcase 
+					
+					instr_xtn.rs2=instruction[24:20];
+					instr_xtn.rs1=instruction[19:15];
+					instr_xtn.imm[4:0]=instruction[11:7];
+					instr_xtn.funct3=instructon[14:12];
+					instr_xtn.imm[11:15]=instruction[31:25];
+					
+
+				
